f31cf418440b3e81ae03afe1d6b706c0f71b1c3b
carlos <gonzalezcgg1@gmail>
2021-04-14 22:03:17 -0500

    rough implementation of backend api

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,8 @@
 # 17776-final-project
 
+admin username: admin
+admin password: password
+
 TODO: discuss api endpoints with front end
+
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import Merchant, Item
+from rest_framework import generics
+from .serializers import MerchantSerializer, ItemSerializer
 
-# Create your views here.
+class ListMerchant(generics.ListAPIView):
+    queryset = Merchant.objects.all()
+    serializer_class = MerchantSerializer
+
+class DetailMerchant(generics.RetrieveAPIView):
+    queryset = Merchant.objects.all()
+    serializer_class = MerchantSerializer
+
+class ListItem(generics.ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class DetailItem(generics.RetrieveAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
