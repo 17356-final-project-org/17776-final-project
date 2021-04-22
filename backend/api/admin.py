a024d39cc4d6baffc3f04af0ee50c33b9434d897
Adam Zhang <yufanz@andrew.cmu.edu>
2021-04-22 19:29:30 -0400

    published boilerplate to adams-django-app.azurewebsites.net

--- backend/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Merchant, Item
-
-admin.site.register(Merchant)
-admin.site.register(Item)
--- backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import # TODO: import the needed views
-
-urlpatterns = [
-    path() # TODO setup path with reference to view
-]
