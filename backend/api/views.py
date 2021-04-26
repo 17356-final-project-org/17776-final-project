0069430849e394efd702976d832acc8801d97071
carlos <gonzalezcgg1@gmail>
2021-04-25 19:13:30 -0500

    updated views and serializer to remove merchant and use new fields from new item model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,16 @@
-from .models import Merchant, Item
+from .models import Item
 from rest_framework import generics
-from .serializers import MerchantSerializer, ItemSerializer
-
-class ListMerchant(generics.ListAPIView):
-    queryset = Merchant.objects.all()
-    serializer_class = MerchantSerializer
-
-class DetailMerchant(generics.RetrieveAPIView):
-    queryset = Merchant.objects.all()
-    serializer_class = MerchantSerializer
+from .serializers import ItemSerializer
 
 class ListItem(generics.ListAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
 class DetailItem(generics.RetrieveAPIView):
+    # TODO (Diego): this view is passed in the name of the item that got scraped
+    # by the front end. It gets passed in the name through the url like so:
+    # api/item/<str:name_of_item_scraped_from_frontend>
+    # see the urls.py file in this app for details. It then needs to use this
+    # passed in string to do the keyword search.
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
