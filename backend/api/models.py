0159a28da9d245a3bea5983fa1a351532d118a2a
carlos <gonzalezcgg1@gmail>
2021-04-25 19:11:38 -0500

    changed models to remove merchant and keep the needed parts of item. also changed fields of item. then made migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 
-class Merchant(models.Model):
-    name = models.CharField(max_length = 50)
-
-    def __str__(self):
-        return self.name
-
 class Item(models.Model):
     name = models.CharField(max_length = 50)
-    category = models.CharField(max_length = 100)
-    merchant = models.ForeignKey(Merchant, on_delete = models.CASCADE)
+    nominal_price = models.FloatField()
+    lowest_price = models.FloatField()
+    item_url = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
