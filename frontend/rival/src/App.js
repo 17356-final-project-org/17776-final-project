712e6f8a602372f1bec905b3550ebcd43f4eedac
Adam Zhang <yufanz@andrew.cmu.edu>
2021-04-26 20:59:50 -0400

    Added state and axios

--- frontend/rival/src/App.js
+++ frontend/rival/src/App.js
@@ -1,21 +1,51 @@
+import React, { useEffect } from 'react';
+
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("Name");
+  const [curPrice, setCurPrice] = useState(0);
+  const [newPrice, setNewPrice] = useState(0);
+  const [oldPrice, setOldPrice] = useState(0);
+  const [link, setLink] = useState("");
+
+  // Get information about current product
+
+  const fetchName = async () => {
+    const res = await axios(``); // TODO: call to Amazon with URL
+    setName(res);
+  }
+
+  const fetchCurPrice = async () => {
+    const res = await axios(``); // TODO: call to Amazon with URL
+    setCurPrice(res);
+  }
+
+  const fetchPricesAndLink = async () => {
+    const res = await axios(``); // TODO: call to back-end with parameter
+    setNewPrice(res.newPrice);
+    setOldPrice(res.oldPrice);
+    setLink(res.link);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name}, {oldPrice}, {newPrice}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={link}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Buy
         </a>
       </header>
     </div>
