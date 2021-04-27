88c65748be5d5bcbe9c9244d00468a510f533a59
Diego Martinez <diegomtzg96@gmail.com>
2021-04-27 18:49:13 -0500

    [WIP] Preliminary algorithm for item keyword matching

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,3 +14,38 @@ class DetailItem(generics.RetrieveAPIView):
     # passed in string to do the keyword search.
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+    # TODO: How to return item returned from bestKeywordMatch?
+
+    def bestKeywordMatch(self, queryItem, minMatches=1):
+        """
+        Finds item whose name most closely matches query item. Since some keywords may exist in
+        two items that are not the same, a minimum threshold of matches must be met to get a result.
+        @param queryItem: name of query item
+        @param minMatches: minimum number of keyword matches to return a result
+        """
+        queryWords = set(queryItem.split(" "))
+
+        for item in Item.objects.all():
+            matches = self.getNumMatchingKeywords(queryItem, item.name)
+            if mostMatches is None or mostMatches < matches:
+                mostMatches = matches
+                bestMatch = item
+
+        if mostMatches >= minMatches:
+            return bestMatch
+        else:
+            return None
+
+    def getNumMatchingKeywords(self, queryName, itemName):
+        """
+        Returns the number of matching keywords between two names.
+        """
+        matches = 0
+        queryWords = set(queryName.split(" "))
+
+        for word in itemName.split(" "):
+            if word in queryWords:
+                matches += 1
+
+        return matches
