33d4e072553b8fd71a3633192098d87eb01ec280
Nicasio <jianeng@hotmail.com>
2021-05-13 23:24:33 -0400

    several fixes: 1) ignore symbols in product name when making api call so that it does not break, 2) inform user when page is not ready, and prevent extension from crashing, 3) if page is ready, allow the query to take place

--- frontend/rival/public/content.js
+++ frontend/rival/public/content.js
@@ -12,16 +12,35 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
         // console.log(document.getElementById("productTitle").childNodes[0].nodeValue)
 
         //returning response to background.js
-        sendResponse(document.getElementById("productTitle").childNodes[0].nodeValue);
+        sendResponse(document.getElementById("productTitle").childNodes[0].nodeValue.trim());
 
         //store in chrome storage so that React can access it
-        var product_title = document.getElementById("productTitle").childNodes[0].nodeValue
-        console.log(product_title)
-        chrome.storage.local.set({'product_title': product_title}, function(result) {
-            console.log("Data saved in chrome storage from content.js")
-        }
-        );
+        var product_title = document.getElementById("productTitle").childNodes[0].nodeValue.trim()
 
+        console.log("over here")
+        console.log(product_title)
+
+        // var product_price = document.getElementById("priceblock_ourprice").childNodes[0].nodeValue.trim()
+
+        // console.log(product_price)
+        // chrome.storage.sync.set({'product_title': product_title, 'product_price': product_price}, function(result) {
+
+        if (product_title === undefined)
+        {
+            chrome.storage.sync.set({'product_title': 'page is still loading please try again'}, function(result) {
+                console.log("Data saved in chrome storage from content.js")
+                console.log("data saved is" + product_title)
+            }
+            );
+        }
+        else
+        {
+            chrome.storage.sync.set({'product_title': product_title}, function(result) {
+                console.log("Data saved in chrome storage from content.js")
+                console.log("data saved is" + product_title)
+            }
+            );
+        }
     }
 
 });
--- frontend/rival/public/popup.js
+++ frontend/rival/public/popup.js
@@ -1,3 +1,26 @@
 //sending message to background js that popup is open
-chrome.runtime.sendMessage({popupOpen: true});
 
+var isPinged = false;
+
+function ping() {
+    chrome.runtime.sendMessage({popupOpen: true}, response => {
+        console.log(chrome.runtime.lastError);
+        if (!isPinged)
+            setTimeout(ping, 1000);
+        //chrome.runtime.sendMessage({popupOpen: true});
+    });
+  }
+
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    console.log(message);
+    if(message.isPinged) {
+      isPinged = true;
+    }
+  });
+
+  chrome.storage.sync.set({'product_title': 'page is still loading please try again'}, function(result) {
+    console.log("first Data saved in chrome storage from content.js")
+    }
+    );
+ping();
+  
